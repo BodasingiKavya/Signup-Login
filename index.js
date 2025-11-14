592d869e292b2c91e3bd1cd0447e72d7bf49e833
kavyabodasingi <kavyagpm123@gmail.com>
2025-11-14 20:20:46 +0530

    sign up and login  functions

--- index.js
+++ index.js
@@ -13,6 +13,33 @@ function signup(username) {
         return 'Signup Successful,Please Login';
     }
 }
+function login(userName, password) {
+    let arr=[
+        'Jone',
+        'Smith',
+        'Alice',
+        'Bob',
+        'Charlie',
+    ];
+    let pass=[
+        'password123',
+        'securepass',
+        'mypassword',
+        'strongpass',
+        'Emp@123'
+    ];
+    
+    let index=arr.indexOf(userName);
+    if(index!==-1 && pass[index]==='Emp@123'){
+        return 'Login Successful...';
+    }
+    else if(index===-1){
+        return 'Wrong Password...';
+    }
+    else{
+        return 'User No Found , Please Signup';
+    }
+}
 
-console.log(signup('Alice','password123'));
-console.log(signup('Charlie','mypassword'));
+console.log(signup('Alice',));
+console.log(login('Charlie','mypassword'));
